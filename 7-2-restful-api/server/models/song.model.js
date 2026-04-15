af00f3bfa71b534d89cbc4ee0f8dbd93ccd97c45
RaneemAlshahrani <raneemal14251026@gmail.com>
2026-04-15 11:52:24 +0300

    update song model year validation and upgrade dependencies

--- 7-2-restful-api/server/models/song.model.js
+++ 7-2-restful-api/server/models/song.model.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 const songSchema = new mongoose.Schema({
     title:  { type: String, required: true, trim: true },
     artist: { type: String, required: true, trim: true },
-    year:   { type: Number, min: 1000, max: 2023 }
+    year:   { type: Number, min: 1000, max: 3000 }
 }, { timestamps: true });
 
 export const Song = mongoose.model("Song", songSchema);
